52b780f62dc97198f24193cf892ccba8616c80ec
Stefanos <stefveisak@gmail.com>
2022-01-05 23:10:14 +0200

    create company register page

--- ccc_webapp/src/main/webapp/ccc_frontend/ccc_frontend/src/register/customer.js
+++ ccc_webapp/src/main/webapp/ccc_frontend/ccc_frontend/src/register/customer.js
@@ -1,5 +1,71 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import useStyles from './styles';
+
 function CustomerRegister() {
-  return <div className="App"></div>;
+  const styles = useStyles();
+  const [Username, setUsername] = useState('');
+  const [Password, setPassword] = useState('');
+  const [Phone, setPhone] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Address, setAddress] = useState('');
+  const [CompanyName, setCompanyName] = useState('');
+
+  // const handleSubmit = (e) => {};
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.textFieldContainer}>
+        <TextField
+          label="Username:"
+          variant="filled"
+          value={Username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <TextField
+          label="Password:"
+          variant="filled"
+          value={Password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <TextField
+          label="Phone:"
+          variant="filled"
+          value={Phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+        />
+        <TextField
+          label="Email:"
+          variant="filled"
+          value={Email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <TextField
+          label="Address:"
+          variant="filled"
+          value={Address}
+          onChange={(e) => setAddress(e.target.value)}
+          required
+        />
+        <TextField
+          label="Company Name:"
+          variant="filled"
+          value={CompanyName}
+          onChange={(e) => setCompanyName(e.target.value)}
+          required
+        />
+        <div className={styles.buttonContainer}>
+          <Button type="submit" variant="contained" color="primary">
+            Signup
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CustomerRegister;
