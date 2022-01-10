ac9e1c92bdf32ae53b10f46a2d468f36e5dbd800
Panagiotis Karouzakis <csd3599@csd.uoc.gr>
2022-01-10 18:56:41 +0200

    updated OpenCompanyAccount

--- ccc_webapp/src/main/java/Servlets/OpenCompanyAccount.java
+++ ccc_webapp/src/main/java/Servlets/OpenCompanyAccount.java
@@ -9,6 +9,7 @@ import hy360.ccc.model.Company;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author panos
  */
+@WebServlet(name = "OpenCompanyAccount", urlPatterns = {"/OpenCompanyAccount"})
 public class OpenCompanyAccount extends HttpServlet {
 
     /**
