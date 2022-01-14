5673b8383e0f0fcba756ac7b6e3eca5f56decfc3
S-C-Koukouzelis <sc.koukouzelis@gmail.com>
2022-01-14 20:15:14 +0200

    Fix on model employee, boughtproduct & product

--- ccc_webapp/src/main/java/hy360/ccc/model/BoughtProduct.java
+++ ccc_webapp/src/main/java/hy360/ccc/model/BoughtProduct.java
@@ -10,16 +10,23 @@ package hy360.ccc.model;
  */
 public class BoughtProduct {
     private String transaction_id;
-    private String bought_product_id;
+    private String product_id;
+    private String merchant_id;
+    private String total;
     
     public BoughtProduct(){
         transaction_id = "";
-        bought_product_id = "";
+        product_id = "";
+        merchant_id = "";
+        total = "";
     }
     
-    public BoughtProduct(String transaction, String bought_prod){
-        transaction_id = transaction;
-        bought_product_id = bought_prod;
+    public BoughtProduct(String transaction, String bought_prod,
+            String merchant, String total){
+        this.transaction_id = transaction;
+        this.product_id = bought_prod;
+        this.merchant_id = merchant;
+        this.total = total;
         
     }
     
@@ -27,48 +34,56 @@ public class BoughtProduct {
         
         
         if(transaction_id == null || transaction_id.trim().isEmpty()
-            || bought_product_id == null || bought_product_id.trim().isEmpty()){
+            || product_id == null || product_id.trim().isEmpty()
+            || merchant_id == null || merchant_id.trim().isEmpty()
+            || total == null || total.trim().isEmpty()    ){
             
             throw new Exception("Missing Fields");
         }
     }
-    
-    
-    
-    public String toString(){
-        StringBuilder ss = new StringBuilder();
-        ss.append("transaction_id: ").append(transaction_id).append("\n")
-                .append("bought_product_id: ").append(bought_product_id).append("\n");
-       
-        return ss.toString();
-    }
 
-    /**
-     * @return the transaction_id
-     */
     public String getTransaction_id() {
         return transaction_id;
     }
 
-    /**
-     * @param transaction_id the transaction_id to set
-     */
     public void setTransaction_id(String transaction_id) {
         this.transaction_id = transaction_id;
     }
 
-    /**
-     * @return the bought_product_id
-     */
-    public String getBought_product_id() {
-        return bought_product_id;
+    public String getProduct_id() {
+        return product_id;
     }
 
-    /**
-     * @param bought_product_id the bought_product_id to set
-     */
-    public void setBought_product_id(String bought_product_id) {
-        this.bought_product_id = bought_product_id;
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public String getMerchant_id() {
+        return merchant_id;
+    }
+
+    public void setMerchant_id(String merchant_id) {
+        this.merchant_id = merchant_id;
+    }
+
+    public String getTotal() {
+        return total;
+    }
+
+    public void setTotal(String total) {
+        this.total = total;
     }
     
+    
+    
+    @Override
+    public String toString(){
+        StringBuilder ss = new StringBuilder();
+        ss.append("transaction_id: ").append(transaction_id).append("\n")
+                .append("product_id: ").append(product_id).append("\n")
+                .append("merchant_id: ").append(merchant_id).append("\n")
+                .append("total: ").append(total).append("\n");
+       
+        return ss.toString();
+    }
 }
