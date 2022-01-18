6808efa9687846cbcd6909db5710fa1fdb79bc1f
Panagiotis Karouzakis <csd3599@csd.uoc.gr>
2022-01-18 13:14:42 +0200

    getCitizen partial

--- ccc_frontend/ccc_frontend/src/citizen/index.js
+++ ccc_frontend/ccc_frontend/src/citizen/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { Typography } from '@mui/material';
 import useStyles from './styles';
 // import { useNavigate } from 'react-router-dom';
@@ -7,6 +7,31 @@ import useStyles from './styles';
 function Citizen() {
   //   const navigate = useNavigate();
   const styles = useStyles();
+  const [firstName, getFirstName] = useState('');
+  const [lastName, getLastName] = useState('');
+  const [amka, getAmka] = useState('');
+  const [vat, getVat] = useState('');
+  const [birthDate, getBirthDate] = useState('');
+  const [gender, getGender] = useState('');
+  const [phone, getPhone] = useState('');
+  const [address, getAddress] = useState('');
+  const [amountDue, getAmountDue] = useState('');
+  const [creditLimit, getCreditLimit] = useState('');
+
+
+
+
+  const getCitizens = async () => {
+    await cccClient
+      .post('getCitizens', `userid=${userid}`)
+      .then(function (response) {
+        console.log(response);
+        setProducts(response.data);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
 
   return (
     <div className={styles.container}>
@@ -14,6 +39,54 @@ function Citizen() {
         <Typography alignSelf={'center'} variant="h4">
           Citizen
         </Typography>
+        <Box pt={3}>
+          <TextField
+            label="FirstName:"
+            variant="readOnly"
+            value={firstName}
+            onChange={(e) => getFirstName(e.target.value)}
+            required
+          />{' '}
+        </Box>
+        <Box pt={3}>
+          <TextField
+            label="LastName:"
+            variant="readOnly"
+            value={price}
+            onChange={(e) => getLastName(e.target.value)}
+            required
+          />{' '}
+        </Box>
+        <Box pt={3}>
+          <TextField
+            label="Amka :"
+            variant="readOnly"
+            type="number"
+            value={quantity}
+            onChange={(e) => getAmka(e.target.value)}
+            required
+          />{' '}
+        </Box>
+        <Box pt={3}>
+          <TextField
+            label="Amka :"
+            variant="readOnly"
+            type="number"
+            value={quantity}
+            onChange={(e) => getAmka(e.target.value)}
+            required
+          />{' '}
+        </Box>
+        <Box pt={3}>
+          <TextField
+            label="Amka :"
+            variant="readOnly"
+            type="number"
+            value={quantity}
+            onChange={(e) => getAmka(e.target.value)}
+            required
+          />{' '}
+        </Box>
       </div>
     </div>
   );
