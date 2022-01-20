5fd97b16d5e8daca8e17afd4f33196049cc4d752
Panagiotis Karouzakis <csd3599@csd.uoc.gr>
2022-01-20 15:18:23 +0200

    finished EmployeeTransactions servlet not tested

--- ccc_webapp/src/main/java/Servlets/Questions/EmployeeTransactions.java
+++ ccc_webapp/src/main/java/Servlets/Questions/EmployeeTransactions.java
@@ -5,8 +5,18 @@
 package Servlets.Questions;
 
 import com.google.gson.Gson;
+import hy360.ccc.db.CompanyDB;
+import hy360.ccc.db.CompanyTradesDB;
+import hy360.ccc.db.MerchantDB;
+import hy360.ccc.db.TransactionDB;
+import hy360.ccc.model.CM_Traffics;
+import hy360.ccc.model.Merchant;
+import hy360.ccc.model.Transaction;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -76,8 +86,36 @@ public class EmployeeTransactions extends HttpServlet {
 
         Gson gson = new Gson();
         String str;
+
+        Map<String, String> map = new HashMap<>();
+        int index = 1;
+        String employee_list = request.getParameter("employeesList");
+        List<CM_Traffics> comp_trades_by_employees = CompanyTradesDB.
+                getTradesByEmployees(employee_list);
+
+        for (CM_Traffics trade : comp_trades_by_employees) {
+            Transaction tr = TransactionDB.getTransaction(trade.getTransaction_id());
+            String company_name = CompanyDB.getCompany("USERID",
+                    trade.getCompany_id()).getName();
+            Merchant mer = MerchantDB.getMerchant("MERCHANT_USERID",
+                    trade.getMerchant_id());
+            String merchant_name = mer.getFirst_name() + mer.getLast_name();
+            String date = tr.getDate();
+            String amount = tr.getAmount();
+            String type = tr.getTransaction_type();
+            map.put(String.valueOf(index), " { company_name: " + company_name
+                    + "merchant_name: " + merchant_name + "date: " + date
+                    + "amount: " + amount + "type:" + type + "}");
+
+        }
+
     
-    
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(200);
+        str = gson.toJson(map);
+        response.getWriter().print(str);
+
     
     }
 
