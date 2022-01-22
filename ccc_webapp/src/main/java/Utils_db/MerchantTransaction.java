d1c660c81d2374ceb0d5d94ad51a55a4bd11e4b7
Panagiotis Karouzakis <csd3599@csd.uoc.gr>
2022-01-22 22:50:50 +0200

    bug?

--- ccc_webapp/src/main/java/Utils_db/MerchantTransaction.java
+++ ccc_webapp/src/main/java/Utils_db/MerchantTransaction.java
@@ -120,4 +120,18 @@ public class MerchantTransaction {
     public void setType(String type) {
         this.type = type;
     }
+
+    /**
+     * @return the employee_name
+     */
+    public String getEmployee_name() {
+        return employee_name;
+    }
+
+    /**
+     * @param employee_name the employee_name to set
+     */
+    public void setEmployee_name(String employee_name) {
+        this.employee_name = employee_name;
+    }
 }
