909b8ae2b61e9c8f06fa87826d791e52215cddb6
Stefanos <stefveisak@gmail.com>
2022-01-23 23:00:43 +0200

    add amount due filtering at company transactions

--- ccc_frontend/ccc_frontend/src/company/transactionHistory.js
+++ ccc_frontend/ccc_frontend/src/company/transactionHistory.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Button, Box, TextField, Select, MenuItem } from '@mui/material';
+import {
+  Typography,
+  Button,
+  Box,
+  TextField,
+  Select,
+  MenuItem,
+  Collapse,
+  ToggleButton
+} from '@mui/material';
 import AdapterDay from '@mui/lab/AdapterDayjs';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
@@ -29,9 +38,12 @@ function TransactionHistory() {
   const navigate = useNavigate();
   const { state } = useLocation();
   const { userid } = state;
+  const [isChecked, setIsChecked] = useState(false);
   const [transactions, setTransactions] = useState([]);
   const [fromDate, setFromDate] = useState(null);
   const [toDate, setToDate] = useState(null);
+  const [fromAmount, setFromAmount] = useState(0);
+  const [toAmount, setToAmount] = useState(0);
   const [personName, setPersonName] = React.useState([]);
   const [employees, setEmployees] = useState([]);
   const styles = useStyles();
@@ -50,10 +62,13 @@ function TransactionHistory() {
   };
 
   const handleClear = () => {
-    setPersonName(null);
+    setPersonName([]);
     setFromDate(null);
     setToDate(null);
-    handleSearch();
+    setFromAmount(0);
+    setToAmount(0);
+    setIsChecked(false);
+    getTransactions();
   };
 
   const getEmployees = async () => {
@@ -78,9 +93,11 @@ function TransactionHistory() {
     await cccClient
       .post(
         'CompanyTransactions',
-        `userId=${userid}&from=${fromDate ? fromDate.toISOString().slice(0, 10) : 'null'}&to=${
-          toDate ? toDate.toISOString().slice(0, 10) : 'null'
-        }&employeesList=${personName.length > 0 ? personName.toString() : 'null'}`
+        `userId=${userid}&fromDate=${
+          fromDate ? fromDate.toISOString().slice(0, 10) : 'null'
+        }&toDate=${toDate ? toDate.toISOString().slice(0, 10) : 'null'}&employeesList=${
+          personName.length > 0 ? personName.toString() : 'null'
+        }&fromAmount=${fromAmount ? fromAmount : 'null'}&toAmount=${toAmount ? toAmount : 'null'}`
       )
       .then(function (response) {
         setTransactions(response.data);
@@ -120,67 +137,118 @@ function TransactionHistory() {
         </Typography>
       </Box>
       <div className={styles.filterContainer}>
-        <Box p={3}>
-          <LocalizationProvider dateAdapter={AdapterDay}>
-            <DesktopDatePicker
-              label="From :"
-              value={fromDate}
-              onChange={handleFromDate}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </Box>
-        <Box p={3}>
-          <LocalizationProvider dateAdapter={AdapterDay}>
-            <DesktopDatePicker
-              label="To :"
-              value={toDate}
-              onChange={handleToDate}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </Box>
-        <Box p={3}>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={personName}
-            multiple
-            label="Employees"
-            onChange={handleChange}
-            MenuProps={MenuProps}
-            sx={{ m: 1, width: 300 }}>
-            {employees.map((row) => (
-              <MenuItem key={row.employee_id} value={row.employee_id}>
-                <Typography>
-                  {row.first_name} {row.last_name}
-                </Typography>
-              </MenuItem>
-            ))}
-          </Select>
-        </Box>
-        <Box p={3} display="flex" alignItems="center">
-          <Button
-            type="primary"
-            variant="contained"
-            color="primary"
-            onClick={() => {
-              handleSearch();
+        <Box p={3} sx={3} display="flex" justifyContent="center" alignItems="center">
+          <ToggleButton
+            value="check"
+            selected={isChecked}
+            onChange={() => {
+              setIsChecked(!isChecked);
             }}>
-            Search
-          </Button>
+            <Typography variant="body"> Filter</Typography>
+          </ToggleButton>
         </Box>
-        <Box p={3} display="flex" alignItems="center">
-          <Button
-            type="primary"
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              handleClear();
-            }}>
-            Clear
-          </Button>
-        </Box>
+        <Collapse in={isChecked}>
+          <Box
+            p={3}
+            sx={3}
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="center"
+            flexDirection={'column'}>
+            <Box display="flex">
+              <Box display="flex" alignItems="center">
+                <Typography variant="body">Date :</Typography>
+              </Box>
+              <Box p={3}>
+                <LocalizationProvider dateAdapter={AdapterDay}>
+                  <DesktopDatePicker
+                    label="From :"
+                    value={fromDate}
+                    onChange={handleFromDate}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </Box>
+              <Box p={3}>
+                <LocalizationProvider dateAdapter={AdapterDay}>
+                  <DesktopDatePicker
+                    label="To :"
+                    value={toDate}
+                    onChange={handleToDate}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
+              </Box>
+            </Box>
+            <Box display="flex">
+              <Box display="flex" alignItems="center">
+                <Typography variant="body">Amount due :</Typography>
+              </Box>
+              <Box p={3}>
+                <TextField
+                  label="From :"
+                  variant="filled"
+                  type="number"
+                  value={fromAmount}
+                  onChange={(e) => setFromAmount(e.target.value)}
+                />
+              </Box>
+              <Box p={3}>
+                <TextField
+                  label="To :"
+                  variant="filled"
+                  type="number"
+                  value={toAmount}
+                  onChange={(e) => setToAmount(e.target.value)}
+                />
+              </Box>
+            </Box>
+            <Box p={3}>
+              <Typography variant="body">Employees :</Typography>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={personName}
+                multiple
+                label="Employees"
+                onChange={handleChange}
+                MenuProps={MenuProps}
+                sx={{ m: 1, width: 300 }}>
+                {employees.map((row) => (
+                  <MenuItem key={row.employee_id} value={row.employee_id}>
+                    <Typography>
+                      {row.first_name} {row.last_name}
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Select>
+            </Box>
+            <Box display="flex">
+              <Box p={3} display="flex" alignItems="center">
+                <Button
+                  type="primary"
+                  variant="contained"
+                  color="primary"
+                  onClick={() => {
+                    handleSearch();
+                  }}>
+                  Search
+                </Button>
+              </Box>
+              <Box p={3} display="flex" alignItems="center">
+                <Button
+                  type="primary"
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => {
+                    handleClear();
+                  }}>
+                  Clear
+                </Button>
+              </Box>
+            </Box>
+          </Box>
+        </Collapse>
       </div>
       <Box p={3} sx={3} display="flex" justifyContent="center" alignItems="center">
         <TableContainer component={Paper}>
